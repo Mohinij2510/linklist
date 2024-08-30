91dcf378fb1a4b3a27f3532c6dfbdb360056a9f4
Mohinij2510 <jmohini951@gmail.com>
2024-08-30 09:56:02 +0530

    Add files via upload

--- ass3.cpp
+++ ass3.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
 using namespace std;
 
-struct node
-{
+struct node{
+	string artist;
 	string title;
+	double dur;
 	node *next;
 };
 	node *header=NULL ;
 	
 void Add_song(){
 	node *nn;
-	string song_name;
+	string song_name,song_artist;
+	double time;
 	nn=new node;
-	cout<<"Enter title of a song  ";
-	cin>>song_name;
+	cout<<"Enter title of a song  ,Name of the artist, Song duration";
+	cin>>song_name>>song_artist>>time;
 	nn->title=song_name;
+	nn->artist=song_artist;
+	nn->dur=time;
 	nn->next=NULL;
 	if(header==NULL){
 		header=nn;
@@ -33,17 +37,48 @@ void display(){
 	song=header;
 	while(song!=NULL){
 		cout<<"Title "<<song->title<<endl;
+		cout<<"Artist: "<<song->artist<<endl;
+		cout<<"Duration: "<<song->dur<<endl;
 		song=song->next;
 	}
+}
+void search(string s){
+	node*song;
+	song=header;
+	while(song->title!=s){
+		song=song->next;
+	}
+	cout<<"Playing "<<song->artist<<endl<<"song"<<song->title<<endl<<"Duration"<<song->dur<<endl;
+}
+void Removal(){
+	node *song;
+	song=header;
+	node *prev=NULL;
+	string s;
+	cout<<"enter the song you want to delete";
+	cin>>s;
+	while(song->title!=s)
+	{
+		prev=song;
+		song=song->next;
+	}
+	prev->next=song->next;
 }
 int main(){
 	node n1;
 	int n;
+	string s;
 	cout<<"Enter number of songs in playlist";
 	cin>>n;
 	for(int i=0;i<n;i++){
-		Add_song();
-	}
+		Add_song();}
+	cout<<"Display playlist "<<endl;
+	display();
+	cout<<"Enter song you want to play";
+	cin>>s;
+	search(s);
+	Removal();
+	cout<<"Display playlist after removing a song ";
 	display();
 	return 0;
 }
